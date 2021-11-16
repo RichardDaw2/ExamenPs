8a40123a6ffc17d530c585e9a5eb9af47540cba0
Richard Eguaras <reguarasbdaw1@gmail.com>
2021-11-16 11:05:04 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var prog = @"C:\Windows\system32\notepad.exe";
+            Console.WriteLine("Inicia el proceso el argumento");
+            Process.Start(prog);
+            
+      
+        }
+    }
+}
